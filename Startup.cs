8661ad75c9deb256aec7deb3eddc19e1f611a484
Dan Karlinski <dan@karlinski.com>
2018-03-26 07:25:00 -0500

    Added webpage to the project, so we can begin integration testing.

--- Startup.cs
+++ Startup.cs
@@ -28,14 +28,22 @@ namespace VideoStoreApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app,
+                      IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                          name: "default",
+                          template: "{ controller = Home}/{ action = Index}/{ id ?}");
+                }
+              );
         }
     }
 }
