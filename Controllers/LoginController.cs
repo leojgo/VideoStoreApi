aaba2c8c91ee071c77a5bc853b91c771e8d1d85c
Administrator <Administrator@CSC478TEAM201>
2018-04-25 20:20:55 -0500

    deleted all references to Context.  these were not/should not have been used, and could be causing issues...

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -10,73 +10,31 @@ namespace VideoStoreApi.Controllers
     [Route("api/Login")]
     public class LoginController : Controller
     {
-        private readonly SessionContext _context;
-
-        public LoginController(SessionContext context)
-        {
-            _context = context;
-        }
-
         //===========================================
         [HttpPost("{id}")]
         public IActionResult Create([FromBody] Login credentials, int id)
         {
-            if (credentials == null && id != 0)
+            if (credentials.Username.ToString() == null || credentials.Password == null)
             {
-                var item = _context.Employees.FirstOrDefault(t => t.EmployeeId == id);
-                if (item == null)
-                {
-                    return NotFound("Employee Was Not Found!");
-                }
-                return new ObjectResult(item);
+                return BadRequest("Username or Password was not provided!");
             }
-            else if(credentials == null)
+
+            var newEmpUtil = new EmployeeUtils();
+
+            try
             {
-                return Json(_context.Employees.ToList());
+                var newEmployee = newEmpUtil.LogIn(credentials.Username, credentials.Password);
+
+                if (newEmployee != null)
+                {
+                    return Json(newEmployee);
+                }
+                return NotFound($"Couldn't Login Employee {credentials.Username}! ");
             }
-            else
+            catch (Exception e)
             {
-                if (credentials.Username.ToString() == null || credentials.Password == null)
-                {
-                    return BadRequest("Username or Password was not provided!");
-                }
-
-                var newEmpUtil = new EmployeeUtils();
-
-                try
-                {
-                    var newEmployee = newEmpUtil.LogIn(credentials.Username, credentials.Password);
-
-                    if (newEmployee != null)
-                    {
-                        if (!checkforSession(credentials.Username))
-                        {
-                            _context.Employees.Add(newEmployee);
-                            _context.SaveChanges();
-                            //return CreatedAtRoute("LoginEmployee", new { id = newEmployee.EmployeeId }, newEmployee);
-                            return Json(newEmployee);
-                        }
-                        return Json(newEmployee);
-
-
-                    }
-                    return NotFound($"Couldn't Login Employee {credentials.Username}! ");
-                }
-                catch (Exception e)
-                {
-                    return NotFound($"Couldn't Login Employee {credentials.Username}! " + e);
-                }
+                return NotFound($"Couldn't Login Employee {credentials.Username}! " + e);
             }
-        }
-
-        public bool checkforSession(int id)
-        {
-            var item = _context.Employees.FirstOrDefault(t => t.EmployeeId == id);
-            if (item == null)
-            {
-                return false;
-            }
-            return true;
         }
     }
 
